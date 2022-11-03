94520bc1db847b920f3844b77b4cc2697fe1de05
MarceloMosl <marcelomosl@icloud.com>
2022-11-02 21:27:34 -0300

    Implementa Função de esconder a primeira tela e mostrar um QUIZZ-Marcelo

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ function getQuizzes(){
          resposta = response.data
         const mostrar = document.querySelector(".todosQuizzes")
         for(i=0;i<resposta.length;i++){
-
             mostrar.innerHTML += `<div class="tQuizz" onclick="displayQuizz(this)"><img class="imgTQuizz" src="${resposta[i].image}" alt="">
                 <span class="nomeTQuizz">${resposta[i].title}</span>
                 <span class="escondido id-doQuizz">${resposta[i].id}</span>
-            </div>
-            `
+            </div>`
         }
     }
     function errQuizzes(response){
@@ -24,14 +22,16 @@ function getQuizzes(){
 getQuizzes();
 function displayQuizz(quizzClicado){
     data = quizzClicado.children[2].innerHTML
-
     const promise = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/"+data)
     promise.then(returnQuizz)
     promise.catch(errIdQuizz)
-
     function returnQuizz(response){
         console.log(response)
 
+        mostraQuizz = document.querySelector(".quizz")
+        mostraQuizz.classList.remove("escondido")
+        escondeFundo = document.querySelector(".main")
+        escondeFundo.classList.add("escondido")
     }
     function errIdQuizz(response){
         console.log(response)
